31a11e05885984a5c63a1a6510941866c37bd8e2
Hirofumi Wakasugi <333180+5t111111@users.noreply.github.com>
2024-01-20 15:24:21 +0900

    bump version

--- wack-log.php
+++ wack-log.php
@@ -4,7 +4,7 @@
  * Plugin Name: WACK Log
  * Plugin URI: https://packagist.org/packages/kodansha/wack-log
  * Description: Simple logger plugin to output logs to stdout.
- * Version: 0.0.1
+ * Version: 0.0.3
  * Author: Kodansha Ltd.
  * Author URI: https://github.com/kodansha
  * License: GPLv3 or later
